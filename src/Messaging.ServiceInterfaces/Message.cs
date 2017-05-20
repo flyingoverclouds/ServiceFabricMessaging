6e430a6b0bfa0658d921621d6f10cd4c0429fff4
Nicolas CLERC <nicolas.clerc@sablefin.net>
2017-05-19 17:27:18 -0700

    initial commit

---
+++ src/Messaging.ServiceInterfaces/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.ServiceInterfaces
+{
+    [DataContract]
+    public class Message
+    {
+        [DataMember]
+        public DateTime MessageDate{ get; set; }
+
+        [DataMember]
+        public string Payload { get; set; }
+    }
+}
